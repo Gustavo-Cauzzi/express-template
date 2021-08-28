7b68f8d98134970cd320ab6af4ff1dccc2529eac
Gustavo-Cauzzi <gustavocauzzi@gmail.com>
2021-08-28 12:03:48 -0300

    Fisrt commit

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const PORT = 3333;
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.json({ status: "up" }).status(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT} 🚀🚀`);
+});
